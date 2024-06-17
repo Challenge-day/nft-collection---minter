a8ec980c613a302615b12a2ff935aa06919a9762
Kingsley Victor <javaprodigy56@gmail.com>
2024-06-17 01:16:28 +0100

    nft standard contract

--- wrappers/ChallengeNFTCollection.compile.ts
+++ wrappers/ChallengeNFTCollection.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/challenge_n_f_t_collection.tact',
+    target: 'contracts/challenge_nfts.tact',
     options: {
         debug: true,
     },
